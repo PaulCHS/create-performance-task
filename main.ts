041d0b2ebc73e3b818707672f4fe87db95246c78
PaulCHS <123192677+PaulCHS@users.noreply.github.com>
2023-04-12 09:26:38 -0700

    Did a bunch of stuff.

--- main.ts
+++ main.ts
@@ -30,45 +30,20 @@ function attack(attackType: string, projectile: Sprite) {
         sprites.destroy(slash)
     } else {
         projectile.follow(enemies[randint(0,enemies.length)])
-        //pause(1000)
-        //sprites.destroy(projectile)
+        pause(1000)
+        sprites.destroy(projectile)
     }
 }
-function createEnemies(enemyType: Image) {
-    let enemy = sprites.create(img`
-        ........................
-        ........................
-        ........................
-        ........................
-        ..........ffff..........
-        ........ff1111ff........
-        .......fb111111bf.......
-        .......f11111111f.......
-        ......fd11111111df......
-        ......fd11111111df......
-        ......fddd1111dddf......
-        ......fbdbfddfbdbf......
-        ......fcdcf11fcdcf......
-        .......fb111111bf.......
-        ......fffcdb1bdffff.....
-        ....fc111cbfbfc111cf....
-        ....f1b1b1ffff1b1b1f....
-        ....fbfbffffffbfbfbf....
-        .........ffffff.........
-        ...........fff..........
-        ........................
-        ........................
-        ........................
-        ........................
-        `, SpriteKind.Enemy)
-    enemy.setPosition(175, 85)
-    enemy.follow(player2, 10)
+let newEnemy = null
+function createEnemy(enemyType: Sprite) {
+    let newEnemy = enemyType
+    newEnemy.setPosition(enemyLocationsX[randint(0, enemyLocationsX.length)], enemyLocationsY[randint(0, enemyLocationsY.length)])
+    newEnemy.follow(player2, 10)
     enemies = sprites.allOfKind(SpriteKind.Enemy)
-    //statusbar.attachToSprite(enemy)
-    return enemy
 }
 info.setLife(3)
 let player2: Sprite = null
+//These are two lists.
 let bolts = [sprites.createProjectileFromSprite(img`
     . . . . . . . . . . . . . . . .
     . . . . . . . . . . . . . . . .
@@ -121,7 +96,7 @@ let bolts = [sprites.createProjectileFromSprite(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, player2, 50, 50)]
-let enemyImages = [img`
+let enemyImages = [sprites.create(img`
     ........................
     ........................
     ........................
@@ -146,7 +121,7 @@ let enemyImages = [img`
     ........................
     ........................
     ........................
-    `, img`
+    `, SpriteKind.Enemy), sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . b 5 b . . . 
     . . . . . . . . . b 5 b . . . . 
@@ -163,33 +138,57 @@ let enemyImages = [img`
     . c d d d d d d 5 5 5 5 5 d b . 
     . . c b d d d d d 5 5 5 b b . . 
     . . . c c c c c c c c b b . . . 
-    `, img`
-    . . f f f . . . . . . . . f f f 
-    . f f c c . . . . . . f c b b c 
-    f f c c . . . . . . f c b b c . 
-    f c f c . . . . . . f b c c c . 
-    f f f c c . c c . f c b b c c . 
-    f f c 3 c c 3 c c f b c b b c . 
-    f f b 3 b c 3 b c f b c c b c . 
-    . c b b b b b b c b b c c c . . 
-    . c 1 b b b 1 b b c c c c . . . 
-    c b b b b b b b b b c c . . . . 
-    c b c b b b c b b b b f . . . . 
-    f b 1 f f f 1 b b b b f c . . . 
-    f b b b b b b b b b b f c c . . 
-    . f b b b b b b b b c f . . . . 
-    . . f b b b b b b c f . . . . . 
-    . . . f f f f f f f . . . . . . 
-    `]
-let enemy = createEnemies(enemyImages[randint(0, enemyImages.length)])
+    `, SpriteKind.Enemy), sprites.create(img`
+        . . f f f . . . . . . . . f f f
+        . f f c c . . . . . . f c b b c
+        f f c c . . . . . . f c b b c .
+        f c f c . . . . . . f b c c c .
+        f f f c c . c c . f c b b c c .
+        f f c 3 c c 3 c c f b c b b c .
+        f f b 3 b c 3 b c f b c c b c .
+        . c b b b b b b c b b c c c . .
+        . c 1 b b b 1 b b c c c c . . .
+        c b b b b b b b b b c c . . . .
+        c b c b b b c b b b b f . . . .
+        f b 1 f f f 1 b b b b f c . . .
+        f b b b b b b b b b b f c c . .
+        . f b b b b b b b b c f . . . .
+        . . f b b b b b b c f . . . . .
+        . . . f f f f f f f . . . . . .
+    `,SpriteKind.Enemy)]
+let enemyLocationsX = [175,24,0]
+let enemyLocationsY = [85,184,0]
+let enemy: Sprite = null
+for (let i = 0; i<7; i++) {
+    enemy = enemyImages[randint(0, enemyImages.length)]
+    createEnemy(enemy)
+}
 let slash: Sprite = null
 let enemies: Sprite[] = []
-//let statusbar = statusbars.create(20, 4, 0)
+let treasure = sprites.create(img`
+    . . b b b b b b b b b b b b . .
+    . b e 4 4 4 4 4 4 4 4 4 4 e b .
+    b e 4 4 4 4 4 4 4 4 4 4 4 4 e b
+    b e 4 4 4 4 4 4 4 4 4 4 4 4 e b
+    b e 4 4 4 4 4 4 4 4 4 4 4 4 e b
+    b e e 4 4 4 4 4 4 4 4 4 4 e e b
+    b e e e e e e e e e e e e e e b
+    b e e e e e e e e e e e e e e b
+    b b b b b b b d d b b b b b b b
+    c b b b b b b c c b b b b b b c
+    c c c c c c b c c b c c c c c c
+    b e e e e e c b b c e e e e e b
+    b e e e e e e e e e e e e e e b
+    b c e e e e e e e e e e e e c b
+    b b b b b b b b b b b b b b b b
+    . b b . . . . . . . . . . b b .
+`, SpriteKind.Player)
+treasure.setPosition(24, 184)
 enemies = sprites.allOfKind(SpriteKind.Enemy)
 tiles.setCurrentTilemap(tilemap`level1`)
 player2 = Render.getRenderSpriteInstance()
 player2.setPosition(232, 25)
-Render.setViewMode(ViewMode.tilemapView)
+Render.setViewMode(ViewMode.raycastingView)
 Render.setAttribute(Render.attribute.dirX, 0)
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite: Sprite, otherSprite: Sprite) {
     info.changeLifeBy(-1)
@@ -197,6 +196,25 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite: Sprite,
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite: Sprite, otherSprite: Sprite) {
     sprites.destroy(otherSprite)
 })
-//statusbars.onZero(0, function(status: StatusBarSprite) {
-//    sprites.destroy(StatusBarSprite.spriteAttachedTo(statusbar))
-//})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite: Sprite, otherSprite: Sprite) {
+    treasure.setImage(img`
+        . b b b b b b b b b b b b b b .
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b
+        b e e 4 4 4 4 4 4 4 4 4 4 e e b
+        b b b b b b b d d b b b b b b b
+        . b b b b b b c c b b b b b b .
+        b c c c c c b c c b c c c c c b
+        b c c c c c c b b c c c c c c b
+        b c c c c c c c c c c c c c c b
+        b c c c c c c c c c c c c c c b
+        b b b b b b b b b b b b b b b b
+        b e e e e e e e e e e e e e e b
+        b e e e e e e e e e e e e e e b
+        b c e e e e e e e e e e e e c b
+        b b b b b b b b b b b b b b b b
+        . b b . . . . . . . . . . b b .
+    `)
+    pause(250)
+    game.gameOver(true)
+})
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,28 +2,40 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100002020200000000000000000000020202020102000000000000000000000200020200020000000000000000000002000202000200000000000000000000020002020002020202020202020202020200020200000000000000000000000000000202020202020202020202020202020202000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
-2 2 2 . . . . . . . . . . 2 2 2 
-2 . 2 . . . . . . . . . . 2 . 2 
-2 . 2 . . . . . . . . . . 2 . 2 
-2 . 2 . . . . . . . . . . 2 . 2 
-2 . 2 2 2 2 2 2 2 2 2 2 2 2 . 2 
-2 . . . . . . . . . . . . . . 2 
+            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101020001020000020000000000010100010300010100010101010101000101000101000201000101010101010001020001010001020001020101010200010100010100000000000000000000000000000101020101010101020101010100010001010101010100010101010102000100010101010102000201010101010001000101010101010000000000000000020001010101010101010102010101000100010100000002010101010101020001000201020200000000000000000000020001010001010102010101000101010100010100000000000000000002000000000101020101010201010101010101020101`, img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.dungeon.chestClosed,sprites.dungeon.floorLight0], TileScale.Sixteen);
+2 . 2 2 . . 2 . . . . . 2 2 . 2 
+2 . 2 2 . 2 2 2 2 2 2 . 2 2 . 2 
+2 . 2 2 . 2 2 2 2 2 2 . 2 2 . 2 
+2 . 2 2 . 2 2 2 2 2 2 . 2 2 . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 . 2 . 2 
+2 2 2 2 2 . 2 2 2 2 2 2 . 2 . 2 
+2 2 2 2 2 . 2 2 2 2 2 2 . 2 . 2 
+2 2 2 2 2 . . . . . . . . 2 . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 . 2 . 2 
+2 . . . 2 2 2 2 2 2 2 2 . 2 . 2 
+2 2 2 . . . . . . . . . . 2 . 2 
+2 . 2 2 2 2 2 2 2 . 2 2 2 2 . 2 
+2 . . . . . . . . . 2 . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.floorLight1,sprites.dungeon.floorLight4,myTiles.tile1], TileScale.Sixteen);
         }
         return null;
     })
@@ -31,6 +43,18 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
         }
         return null;
     })
